33cc46cf63d371b5c84152a34f0da0d237bc2081
Lu Zhen <luzhen@uniontech.com>
2023-08-24 15:52:27 +0800

    feat: add deb auto-pacakge
    add debian folder with config file.
    
    Log: auto-package supported

--- deb-clean.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-script_dir=$(dirname "$(readlink -f "$0")")
-rm -rf $script_dir/deepin-codeporting*
-rm -rf $script_dir/DEBIAN/changelog
-rm -rf $script_dir/usr/
-rm -rf $script_dir/porting-script/deepin-codeporting.spec
-rm -rf $script_dir/porting-script/build/
